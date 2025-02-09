b5c5228a12bbdfd1dbb1f846d0092681ae9d147d
JayJayArr <JayArr@outlook.de>
2025-02-09 17:22:35 +0100

    reduce tapping time

--- .keymap/config.h
+++ .keymap/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 175
+#define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 120
 // #define HOLD_ON_OTHER_KEY_PRESS
