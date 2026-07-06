74fabbc9c1df45dea61860558d75076044ec38e4
JayJayArr <JayArr@outlook.de>
2026-07-06 16:22:51 +0200

    Flow out, hodl in

--- .keymap/config.h
+++ .keymap/config.h
@@ -36,8 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 110
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 110
-#define FLOW_TAP_TERM 110
-// #define CHORDAL_HOLD
+// #define FLOW_TAP_TERM 110
+#define CHORDAL_HOLD
 
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
